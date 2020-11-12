bf70c352159ae9e766c5ac062ec55b4a17f34035
VitMarKha <vitmarkha@icloud.com>
2020-11-12 14:24:44 +0300

    Fix

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fngoc <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/08 12:39:41 by fngoc             #+#    #+#             */
-/*   Updated: 2020/11/08 12:39:43 by fngoc            ###   ########.fr       */
+/*   Created: 2020/11/11 12:18:02 by fngoc             #+#    #+#             */
+/*   Updated: 2020/11/11 12:18:04 by fngoc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <unistd.h>
 # include <stdlib.h>
